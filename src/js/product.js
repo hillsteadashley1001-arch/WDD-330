37c1c5d7578ab8c1ec0096344fa18744c2752fd2
Ashmhill <amh110@byui.edu>
2025-10-05 11:36:15 -0600

    Team 3

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,5 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-const dataSource = new ProductData("tents");
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+productDetails(productId, ".product-detail");
